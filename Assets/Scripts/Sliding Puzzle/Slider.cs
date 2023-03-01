c199f81e617c29e3a2fdc80b129890f2342b82e1
ahmdshahal <ahmdshahal29@gmail.com>
2023-03-01 21:07:19 +0700

    Sliding Puzzle Fix

--- Assets/Scripts/Sliding Puzzle/Slider.cs
+++ Assets/Scripts/Sliding Puzzle/Slider.cs
@@ -1,35 +1,19 @@
-using System;
 using UnityEngine;
 
 public class Slider : MonoBehaviour
 {
-    [SerializeField] private Transform emptySpace;
-    
+    public Vector3 correctPosition;
     public Vector3 targetPosition;
-    public int correctPositionX;
-    public int correctPositionY;
-    public SliderManager sliderManager;
-
-    private bool onSlide;
-
-    public void SlidePuzzle()
+    public bool inRightPlace;
+    
+    private void Awake()
     {
-        targetPosition = transform.position;
-        var distance = Vector3.Distance(emptySpace.position, targetPosition);
-
-        if (distance <= 20)
-        {
-            onSlide = true;
-            (targetPosition, emptySpace.position) = (emptySpace.position, targetPosition);
-            Debug.Log("slided");
-        }
+        targetPosition = transform.localPosition;
+        correctPosition = targetPosition;
     }
 
     private void Update()
     {
-        if (onSlide)
-        {
-            transform.position = Vector3.Lerp(transform.position, targetPosition, 0.05f);
-        }
+        transform.localPosition = Vector2.Lerp(transform.localPosition, targetPosition, 0.05f);
     }
 }
