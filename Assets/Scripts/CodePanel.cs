b84e99ca342b8fa9305f825e312bac6889a69c58
ahmdshahal <ahmdshahal29@gmail.com>
2023-03-04 10:00:19 +0700

    Password Puzzle

---
+++ Assets/Scripts/CodePanel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CodePanel : MonoBehaviour 
+{
+	[SerializeField]
+	private TextMeshProUGUI codeText;
+	private string codeTextValue = "";
+
+	public Animator anim;
+	public GameObject numPanel;
+	public GameObject puzzle;
+	public AudioSource audioSource;
+
+	void Update () 
+	{
+		codeText.text = codeTextValue;
+
+		if (codeTextValue.Length == 4)
+		{
+			if (codeTextValue == "1837")
+			{
+				anim.SetBool("Active", true);
+                puzzle.SetActive(true);
+                numPanel.SetActive(false);
+                codeTextValue = "";
+            }
+			else
+			{
+				codeTextValue = "";
+				audioSource.Play();
+			}
+		}
+	}
+
+	public void AddDigit(string digit)
+	{
+		codeTextValue += digit;
+	}
+
+}
