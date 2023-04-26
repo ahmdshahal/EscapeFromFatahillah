052c9426071f36fcf0c0e384069040627d556206
ahmdshahal <ahmdshahal29@gmail.com>
2023-04-26 19:56:28 +0700

    Fix hint material
    Add comment to make code easy to read and more clean
    Add pick rotation
    Fix hint material not change in object have one more material

--- Assets/Scripts/HintSystem.cs
+++ Assets/Scripts/HintSystem.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,35 +5,51 @@ using UnityEngine;
 public class HintSystem : MonoBehaviour
 {
     public Renderer[] hintObjects; // list dari semua objek hint
-    private Material[] originalMaterials; // list dari material asli dari setiap objek hint
+    private Dictionary<Renderer, Material[]> originalMaterials; // dictionary dari material asli dari setiap objek hint
 
     private void Start()
     {
-        originalMaterials = new Material[hintObjects.Length];
-        for (int i = 0; i < hintObjects.Length; i++)
+        originalMaterials = new Dictionary<Renderer, Material[]>();
+        foreach (Renderer hintObject in hintObjects)
         {
-            originalMaterials[i] = hintObjects[i].material; // menyimpan material asli dari setiap objek hint
+            Material[] materials = hintObject.materials;
+            originalMaterials.Add(hintObject, materials); // menyimpan material asli dari setiap objek hint
         }
     }
 
-    // method untuk mengubah material dari semua objek hint
+    /// <summary>
+    /// Coroutine untuk mengubah material object menjadi hintMaterial selama duration detik
+    /// </summary>
+    /// <param name="hintMaterial">Material hint yang akan digunakan</param>
+    /// <param name="duration">Durasi mengubah material</param>
     public void ChangeHintMaterial(Material hintMaterial, float duration)
     {
         StartCoroutine(ChangeHintMaterialCoroutine(hintMaterial, duration));
     }
-
+    
+    // method untuk mengubah material dari semua objek hint
     private IEnumerator ChangeHintMaterialCoroutine(Material hintMaterial, float duration)
     {
         foreach (Renderer hintObject in hintObjects)
         {
-            hintObject.material = hintMaterial; // mengubah material dari objek hint
+            Material[] materials = hintObject.materials;
+            originalMaterials.TryGetValue(hintObject, out Material[] originalMaterial); // mengambil material asli dari dictionary
+            for (int i = 0; i < materials.Length; i++)
+            {
+                materials[i] = hintMaterial;
+            }
+            hintObject.materials = materials;
         }
 
         yield return new WaitForSeconds(duration);
 
-        for (int i = 0; i < hintObjects.Length; i++)
+        foreach (Renderer hintObject in hintObjects)
         {
-            hintObjects[i].material = originalMaterials[i]; // mengembalikan material objek hint ke material aslinya
+            Material[] materials = hintObject.materials;
+            originalMaterials.TryGetValue(hintObject, out Material[] originalMaterial); // mengambil material asli dari dictionary
+            for (int j = 0; j < materials.Length; j++)
+                if (originalMaterial != null) materials[j] = originalMaterial[j];
+            hintObject.materials = materials;
         }
     }
 }
--- Assets/Scripts/Interactable Objects/DropObject.cs
+++ Assets/Scripts/Interactable Objects/DropObject.cs
@@ -6,38 +6,40 @@ using UnityEngine.SceneManagement;
 
 public class DropObject : Interactable
 {
-    [SerializeField] private PickDropSystem pickDropSystem;
-    [SerializeField] private MainMenuTask mainMenuTask;
-    [SerializeField] private string dropID;
-    [SerializeField] private string LoadToScene;
-    [SerializeField] private int scene;
-    [SerializeField] private Transform dropPoint;
-    [SerializeField] private bool isDoor;
+    [SerializeField] private PickDropSystem pickDropSystem; // variabel untuk mengambil data dari PickDropSystem
+    [SerializeField] private MainMenuTask mainMenuTask; // variabel untuk mengambil data dari MainMenuTask
+    [SerializeField] private string dropID; // id objek yang akan di-drop
+    [SerializeField] private string LoadToScene; // nama scene yang akan dimuat
+    [SerializeField] private int scene; // nomor scene
+    [SerializeField] private Transform dropPoint; // titik drop objek
+    [SerializeField] private bool isDoor; // apakah objek yang di-drop adalah pintu
     
+    // method yang akan dijalankan ketika objek di-interaksi
     protected override void Interact()
     {
-        if (!pickDropSystem.isEmpty)
+        if (!pickDropSystem.isEmpty) // cek apakah ada objek yang sedang dipegang
         {
-            if (pickDropSystem.id == dropID)
+            if (pickDropSystem.id == dropID) // cek apakah objek yang sedang dipegang cocok dengan id yang diperlukan untuk melakukan drop
             {
-                pickDropSystem.id = null;
-                pickDropSystem.isEmpty = true;
+                pickDropSystem.id = null; // reset id objek yang dipegang di PickDropSystem
+                pickDropSystem.isEmpty = true; // set status di PickDropSystem menjadi kosong
 
-                if (isDoor)
-                    SceneManager.LoadScene(LoadToScene);
-                else
+                if (isDoor) // jika objek yang di-drop adalah pintu
+                    SceneManager.LoadScene(LoadToScene); // load scene yang sudah ditentukan
+
+                else // jika objek yang di-drop bukan pintu
                 {
-                    pickDropSystem.pickObject.transform.SetParent(dropPoint);
-                    pickDropSystem.pickObject.transform.position = dropPoint.position;
-                    pickDropSystem.pickObject.transform.rotation = dropPoint.rotation;
+                    pickDropSystem.pickObject.transform.SetParent(dropPoint); // set parent dari objek yang dipegang ke titik drop
+                    pickDropSystem.pickObject.transform.position = dropPoint.position; // set posisi objek yang dipegang ke posisi titik drop
+                    pickDropSystem.pickObject.transform.rotation = dropPoint.rotation; // set rotasi objek yang dipegang ke rotasi titik drop
 
-                    pickDropSystem.pickObject.layer = 0;
+                    pickDropSystem.pickObject.layer = 0; // set layer objek yang di-drop menjadi default
                 }
 
-                switch (scene)
+                switch (scene) // cek nomor scene yang dimuat
                 {
-                    case 0:
-                        mainMenuTask.PuzzleCompleted();
+                    case 0: // jika nomor scene adalah 0
+                        mainMenuTask.PuzzleCompleted(); // jalankan method PuzzleCompleted() pada MainMenuTask
                         break;
                     case 1:
                         
